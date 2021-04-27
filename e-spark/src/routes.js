b5237db5c02e55e0d72f857dfad5017a22d91426
Bruno Silva <brunobsg999@gmail.com>
2021-04-27 20:57:53 -0300

    Atualizei o CSS

--- e-spark/src/routes.js
+++ e-spark/src/routes.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { ScrollCategoryProvider } from '../src/providers/scrollCategory';
+import { ScrollCategoryProvider } from "../src/providers/scrollCategory";
 
-import { AuthProvider } from '../src/providers/auth';
+import { AuthProvider } from "../src/providers/auth";
 
-import { AboutCar } from './pages/AboutCar';
-import { Checkout } from './pages/Checkout';
-import { Pagamento } from './pages/Pagamento';
-import { Planos } from './pages/Planos';
-import { User } from './pages/User';
-import { Home } from './pages/Home';
+import { AboutCar } from "./pages/AboutCar";
+import { Checkout } from "./pages/Checkout";
+import { Pagamento } from "./pages/Pagamento";
+import { Planos } from "./pages/Planos";
+import { dados } from "./pages/Dados";
+import { Home } from "./pages/Home";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <AuthProvider>
-                    <ScrollCategoryProvider>
-                        <Route path="/" exact component={Home} />
-                    </ScrollCategoryProvider>
-                    <Route path="/aboutcar" component={AboutCar} />
-                    <Route path="/Checkout" component={Checkout} />
-                    <Route path="/pagamento" component={Pagamento} />
-                    <Route path="/planos" component={Planos} />
-                    <Route path="/user" component={User} />
-                </AuthProvider>
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <AuthProvider>
+          <ScrollCategoryProvider>
+            <Route path="/" exact component={Home} />
+          </ScrollCategoryProvider>
+          <Route path="/aboutcar" component={AboutCar} />
+          <Route path="/Checkout" component={Checkout} />
+          <Route path="/pagamento" component={Pagamento} />
+          <Route path="/planos" component={Planos} />
+          <Route path="/dados" component={Dados} />
+        </AuthProvider>
+      </Switch>
+    </BrowserRouter>
+  );
 }
